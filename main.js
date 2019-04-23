1bdd7dcf3302247e6561c87ab9b7b8e024cc166e
ferranjay <ferranjay@gmail.com>
2019-04-23 23:31:12 +0200

    css javascript

--- main.js
+++ main.js
@@ -1,33 +1,94 @@
-
-  TweenMax.from(".left-col", 2, {
-        width: "0%",
-        ease: Expo.easeInOut
-  })
-
-  TweenMax.from(".logo", 2, {
-        delay: 2.5,
-        y: 20,
-        opacity: 0,
-        ease: Expo.easeInOut
-  })
-
-  TweenMax.from(".extra", 2, {
-        delay: 2.8,
-        y: 20,
-        opacity: 0,
-        ease: Expo.easeInOut
-  })
-
-  TweenMax.from(".year", 2, {
-        delay: 3.1,
-        y: 20,
-        opacity: 0,
-        ease: Expo.easeInOut
-  })
-
-  TweenMax.from(".title", 2, {
-        delay: 2.4,
-        y: 20,
-        opacity: 0,
-        ease: Expo.easeInOut
-  })
+TweenMax.from(".left-col", 2, {
+      width: "0%",
+      ease: Expo.easeInOut
+})
+TweenMax.from(".logo", 2, {
+      delay: 2.5,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".extra", 2, {
+      delay: 2.8,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".year", 2, {
+      delay: 3.1,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".title", 2, {
+      delay: 2.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".subtitle", 2, {
+      delay: 2.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".about h1", 2, {
+      delay: 3,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".heading-one", 2, {
+      delay: 3.2,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".heading-two", 2, {
+      delay: 3.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".heading-three", 2, {
+      delay: 3.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".media p", 2, {
+      delay: 3.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.staggerFrom(".media ul li", 2, {
+      delay: 3.8,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+}, 0.1);
+TweenMax.from(".heading-four", 2, {
+      delay: 4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".heading-five", 2, {
+      delay: 4.2,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".next", 2, {
+      delay: 4.4,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+TweenMax.from(".more", 2, {
+      delay: 4.6,
+      y: 20,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
