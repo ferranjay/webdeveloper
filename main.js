e10159d8fae2b1301e02c474cbe7e97daeb8d2a5
ferranjay <ferranjay@gmail.com>
2019-04-23 20:00:36 +0200

    first css

---
+++ main.js
@@ -0,0 +1,33 @@
+
+  TweenMax.from(".left-col", 2, {
+        width: "0%",
+        ease: Expo.easeInOut
+  })
+
+  TweenMax.from(".logo", 2, {
+        delay: 2.5,
+        y: 20,
+        opacity: 0,
+        ease: Expo.easeInOut
+  })
+
+  TweenMax.from(".extra", 2, {
+        delay: 2.8,
+        y: 20,
+        opacity: 0,
+        ease: Expo.easeInOut
+  })
+
+  TweenMax.from(".year", 2, {
+        delay: 3.1,
+        y: 20,
+        opacity: 0,
+        ease: Expo.easeInOut
+  })
+
+  TweenMax.from(".title", 2, {
+        delay: 2.4,
+        y: 20,
+        opacity: 0,
+        ease: Expo.easeInOut
+  })
